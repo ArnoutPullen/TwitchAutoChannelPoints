064f2347a843762feb189d07df0de0b340de59a9
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2020-12-03 12:51:40 +0100

    first commit

---
+++ sites/twitch/twitch.js
@@ -0,0 +1,11 @@
+var interval = setInterval(function () {
+
+    var button = document.querySelector('.community-points-summary .tw-z-above button.tw-button');
+
+    if (button) {
+        button.click();
+    }
+
+}, 1000);
+
+// clearInterval(interval);
